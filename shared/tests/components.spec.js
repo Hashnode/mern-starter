ae19275d5b5c0221cc9bf0b144a337c507d0e286
Mayank Chandola <imayankchd@gmail.com>
2016-02-10 13:11:49 +0530

    add more test and refractor

--- shared/tests/components.spec.js
+++ shared/tests/components.spec.js
@@ -2,6 +2,7 @@
 import expect from 'expect';
 import TestUtils from 'react-addons-test-utils';
 import PostListItem from '../components/PostListItem/PostListItem';
+import PostCreateView from '../components/PostCreateView/PostCreateView';
 import React from 'react';
 import expectJSX from 'expect-jsx';
 import { Link } from 'react-router';
@@ -20,9 +21,26 @@ describe('component tests', () => {
     const output = renderer.getRenderOutput();
     expect(output).toEqualJSX(<div className="main">
            <span className="name">Name: {post.name}</span>
-           <Link to={`/post/ + ${post.title}`} onClick={function click() {}}>
+           <Link to={'/post/' + post.title} onClick={function click() {}}>
              <span className="title">Title: {post.title}</span></Link>
            <span className="content">Content: {post.content}</span>
          </div>);
+  });
+
+  it('should render PostCreateView properly', () => {
+    const renderer = TestUtils.createRenderer();
+    const post = {
+      name: 'Prank',
+      title: 'first post',
+      content: 'hello world!',
+    };
+    renderer.render(<PostCreateView addPost={function noop() {}}/>);
+    const output = renderer.getRenderOutput();
+    expect(output).toEqualJSX(<div className="form">
+            <input placeholder="Name" ref="name"/><br/>
+            <input placeholder="Title" ref="title"/><br/>
+            <textarea placeholder="Content" ref="content"></textarea><br/>
+            <button onClick={function noop() {}}>Submit</button>
+          </div>);
   });
 });
