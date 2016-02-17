a15c439717f9f82b8d22d3c6ebc1fa8467e9b780
prank7 <prashant.abhishek7g@gmail.com>
2016-02-17 13:23:21 +0530

    Add CSS minification in build step

--- server/server.js
+++ server/server.js
@@ -50,6 +50,7 @@ app.use('/api', posts);
 
 // Render Initial HTML
 const renderFullPage = (html, initialState) => {
+  const cssPath = process.env.NODE_ENV === 'PRODUCTION' ? '/css/app.min.css' : '/css/app.css';
   return `
     <!doctype html>
     <html>
@@ -58,7 +59,7 @@ const renderFullPage = (html, initialState) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Isomorphic Redux Example</title>
-        <link rel="stylesheet" href="/css/app.css" />
+        <link rel="stylesheet" href=${cssPath} />
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
         <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
       </head>
