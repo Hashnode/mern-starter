ae5dbdbda640759f32b5b609ba3e79e12bc18127
Will Cameron <cameron.will@gmail.com>
2016-02-18 12:04:58 -0800

    copy post.controller to hapi.post.controller.js to be changed later WIP

---
+++ server/controllers/hapi.post.controller.js
@@ -0,0 +1,60 @@
+import Post from '../models/post';
+import cuid from 'cuid';
+import slug from 'slug';
+
+var sanitizeHtml = require('sanitize-html');
+
+export function getPosts(req, res) {
+  Post.find().sort('-dateAdded').exec((err, posts) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({ posts });
+  });
+}
+
+export function addPost(req, res) {
+  if (!req.body.post.name || !req.body.post.title || !req.body.post.content) {
+    return res.status(403).end();
+  }
+
+  var newPost = new Post(req.body.post);
+  
+  //Let's sanitize inputs
+  newPost.title = sanitizeHtml(newPost.title);
+  newPost.name = sanitizeHtml(newPost.name);
+  newPost.content = sanitizeHtml(newPost.content);
+  
+  newPost.slug = slug(newPost.title.toLowerCase(), { lowercase: true });
+  newPost.cuid = cuid();
+  newPost.save((err, saved) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    return res.json({ post: saved });
+  });
+}
+
+export function getPost(req, res) {
+  var slug = req.query.slug.split('-');
+  var cuid = slug[slug.length - 1];
+  Post.findOne({ cuid: cuid }).exec((err, post) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    res.json({ post });
+  });
+}
+
+export function deletePost(req, res) {
+  var postId = req.body.postId;
+  Post.findById(postId).exec((err, post) => {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    post.remove(function () {
+      res.status(200).end();
+    });
+  });
+}
