52d168923fb842be3c49c5a939ba993faffde7b5
Mayank Chandola <imayankchd@gmail.com>
2016-02-18 19:22:49 +0530

    Update fetchData.js

--- server/util/fetchData.js
+++ server/util/fetchData.js
@@ -1,3 +1,5 @@
+/* This was inspired from https://github.com/caljrimmer/isomorphic-redux-app/blob/73e6e7d43ccd41e2eb557a70be79cebc494ee54b/src/common/api/fetchComponentDataBeforeRender.js */
+
 export function fetchComponentData(dispatch, components, params) {
   const needs = components.reduce((prev, current) => {
     return (current.need || [])
