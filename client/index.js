997c48c37ac86ba4fbb449bea5482c53494d2eac
Sandeep Panda <sandeep.panda92@gmail.com>
2016-02-24 12:07:35 +0530

    Removed createBrowserHistory
    No longer needed.

--- client/index.js
+++ client/index.js
@@ -10,8 +10,6 @@ import PostDetailView from '../shared/container/PostDetailView/PostDetailView';
 import { Router, browserHistory, Route, IndexRoute, match } from 'react-router';
 import routes from '../shared/routes';
 
-import createBrowserHistory from 'history/lib/createBrowserHistory';
-
 const store = configureStore(window.__INITIAL_STATE__);
 const history = browserHistory;
 
