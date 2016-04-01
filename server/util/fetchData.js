e45018abb61028237510dc865538df7ff21543d8
Roger Beaman <roger.beaman@Installs-iMac.local>
2016-03-31 20:10:37 -0400

    making sync server fetching conditional on whether or not state is included in needs

--- server/util/fetchData.js
+++ server/util/fetchData.js
@@ -1,4 +1,5 @@
 /* This was inspired from https://github.com/caljrimmer/isomorphic-redux-app/blob/73e6e7d43ccd41e2eb557a70be79cebc494ee54b/src/common/api/fetchComponentDataBeforeRender.js */
+import { stripComments } from './stripComments';
 import { sequence } from './promiseUtils';
 
 export function fetchComponentData(store, components, params) {
@@ -8,5 +9,10 @@ export function fetchComponentData(store, components, params) {
       .concat(prev);
   }, []);
 
-  return sequence(needs, need => store.dispatch(need(params, store.getState())));
+  // Test if component "needs" (with comments removed) contain the string "state", if so they are assumed state dependent and must be fetched synchronously with "sequence"
+  const needsAreStateDependent = stripComments(needs.toString()).indexOf('state') > -1;
+
+  return needsAreStateDependent ?
+         sequence(needs, need => store.dispatch(need(params, store.getState()))) :  // fetch synchronously
+         Promise.all(needs.map(need => store.dispatch(need(params))));              // fetch asynchronously
 }
