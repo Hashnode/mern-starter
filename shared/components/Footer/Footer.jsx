fc70218ca23e480511d77a96a55f51121145041e
amgad_youssef <amgad@apple.com>
2016-05-17 15:13:45 -0700

    Code cleanup

--- shared/components/Footer/Footer.jsx
+++ shared/components/Footer/Footer.jsx
@@ -1,12 +1,12 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Footer extends Component {
-  render() {
-    return (
-      <div className="footer">
-        <p>&copy; 2016 &middot; Hashnode &middot; LinearBytes Inc.</p>
-        <p>We are on Twitter : <a href="https://twitter.com/@mern_io" target="_Blank">@mern_io</a></p>
-      </div>
-    );
-  }
-}
+const Footer = (props, context) => {
+  return (
+    <div className="footer">
+      <p>&copy; 2016 &middot; Hashnode &middot; LinearBytes Inc.</p>
+      <p>We are on Twitter : <a href="https://twitter.com/@mern_io" target="_Blank">@mern_io</a></p>
+    </div>
+  );
+};
+
+export default Footer;
