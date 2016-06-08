02733f4200fffaff848ad614b1ce2d47dd866ad1
Somasundaram Ayyappan <somasundaram321@gmail.com>
2016-06-08 14:48:29 +0530

    fix(hotreloader): add workaround for react-router async routes

--- client/routes.js
+++ client/routes.js
@@ -1,3 +1,4 @@
+/* eslint-disable global-require */
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './modules/App/App';
@@ -7,6 +8,16 @@ if (typeof require.ensure !== 'function') {
   require.ensure = function requireModule(deps, callback) {
     callback(require);
   };
+}
+
+/* Workaround for async react routes to work with react-hot-reloader till
+  https://github.com/reactjs/react-router/issues/2182 and
+  https://github.com/gaearon/react-hot-loader/issues/288 is fixed.
+ */
+if (process.env.NODE_ENV !== 'production') {
+  // Require async routes only in development for react-hot-reloader to work.
+  require('./modules/Post/pages/PostListPage/PostListPage');
+  require('./modules/Post/pages/PostDetailPage/PostDetailPage');
 }
 
 // react-router setup with code-splitting
