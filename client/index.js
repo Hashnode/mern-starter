a138270ea4309e4ac2ae7476d5ae144dd1563940
jaga <kandros5591@gmail.com>
2016-06-08 11:38:36 +0200

    fix client side mismatch markup applying react-router's match to client side

--- client/index.js
+++ client/index.js
@@ -6,17 +6,24 @@ import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 import App from './App';
 import { configureStore } from './store';
+import { browserHistory } from 'react-router';
+import { match } from 'react-router';
+
+// Import Routes
+import routes from './routes';
 
 // Initialize store
 const store = configureStore(window.__INITIAL_STATE__);
 const mountApp = document.getElementById('root');
 
-render(
-  <AppContainer>
-    <App store={store} />
-  </AppContainer>,
-  mountApp
-);
+match({ history: browserHistory, routes }, (error, redirectLocation, renderProps) => {
+  render(
+    <AppContainer>
+      <App store={store} renderProps={renderProps} />
+    </AppContainer>,
+    mountApp
+  );
+});
 
 // For hot reloading of react components
 if (module.hot) {
@@ -24,11 +31,13 @@ if (module.hot) {
     // If you use Webpack 2 in ES modules mode, you can
     // use <App /> here rather than require() a <NextApp />.
     const NextApp = require('./App').default; // eslint-disable-line global-require
-    render(
-      <AppContainer>
-        <NextApp store={store} />
-      </AppContainer>,
-      mountApp
-    );
+    match({ history: browserHistory, routes }, (error, redirectLocation, renderProps) => {
+      render(
+        <AppContainer>
+          <NextApp store={store} renderProps={renderProps} />
+        </AppContainer>,
+        mountApp
+      );
+    })
   });
 }
