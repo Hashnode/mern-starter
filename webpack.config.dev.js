2c49a05ed6160c48e5743e189b8363f8f320c8c1
Somasundaram Ayyappan <somasundaram321@gmail.com>
2016-08-10 14:53:30 +0530

    fix(IE): add eventsource polyfill for hot reloading to work in IE

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -8,6 +8,7 @@ module.exports = {
 
   entry: {
     app: [
+      'eventsource-polyfill',
       'webpack-hot-middleware/client',
       'webpack/hot/only-dev-server',
       'react-hot-loader/patch',
