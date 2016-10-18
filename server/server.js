250a8af14cf067c04212d8616dbbd3aa9c92be14
Jake Whetstone <jake@indrez.com>
2016-10-18 08:35:13 -0400

    Allow parallel apps for multiple concurrent AVA tests. Prevent early mongoose connection in server.js under test environment so that tests can run without a real mongo database available. Await Model.create promise so that tests will not run before test data is created. Altered mockgoose URL to prove mockgoose is not connecting to a real mongoDB server.

--- server/server.js
+++ server/server.js
@@ -39,16 +39,18 @@ import serverConfig from './config';
 // Set native promises as mongoose promise
 mongoose.Promise = global.Promise;
 
-// MongoDB Connection
-mongoose.connect(serverConfig.mongoURL, (error) => {
-  if (error) {
-    console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
-    throw error;
-  }
+// MongoDB Connection; will be initialized later in test environment.
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect(serverConfig.mongoURL, (error) => {
+    if (error) {
+      console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
+      throw error;
+    }
 
-  // feed some dummy data in DB.
-  dummyData();
-});
+    // feed some dummy data in DB.
+    dummyData();
+  });
+}
 
 // Apply body Parser and server public assets and routes
 app.use(compression());
--- server/util/test-helpers.js
+++ server/util/test-helpers.js
@@ -1,11 +1,10 @@
 import mongoose from 'mongoose';
 import mockgoose from 'mockgoose';
 
-export function connectDB(t, done) {
+export function connectDB(t) {
   mockgoose(mongoose).then(() => {
-    mongoose.createConnection('mongodb://localhost:27017/mern-test', err => {
+    mongoose.connect('mongodb://mockhost:1/mern-test', err => {
       if (err) t.fail('Unable to connect to test database');
-      done();
     });
   });
 }
