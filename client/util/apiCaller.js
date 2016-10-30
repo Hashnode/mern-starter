e232173158868b87527becaf5a2f55ddbf2f3b59
Alex Sandiiarov <sandiyarov@gmail.com>
2016-10-29 19:20:21 -0700

    Update apiCaller.js

--- client/util/apiCaller.js
+++ client/util/apiCaller.js
@@ -2,7 +2,7 @@ import fetch from 'isomorphic-fetch';
 import Config from '../../server/config';
 
 export const API_URL = (typeof window === 'undefined' || process.env.NODE_ENV === 'test') ?
-  process.env.BASE_URL || (`http://localhost:${process.env.PORT || Config.port}/api`) :
+  process.env.BASE_URL || (`http://localhost:${Config.port}/api`) :
   '/api';
 
 export default function callApi(endpoint, method = 'get', body) {
