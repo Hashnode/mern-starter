8d379bf60e8e94899b6f4c9107d7bf158e9925e5
Knuckles9090 <jrahko@gmail.com>
2016-11-17 11:23:01 +0000

    Update PostDetailPage.js

--- client/modules/Post/pages/PostDetailPage/PostDetailPage.js
+++ client/modules/Post/pages/PostDetailPage/PostDetailPage.js
@@ -25,7 +25,7 @@ export function PostDetailPage(props) {
   );
 }
 
-// Actions required to provide data for this component to render in sever side.
+// Actions required to provide data for this component to render in server side.
 PostDetailPage.need = [params => {
   return fetchPost(params.cuid);
 }];
