60665053ce481f618fdcb046b282e24fc470ffd6
Pavlo Vozniuk <antipsih@ukr.net>
2017-05-19 17:45:57 +0300

    init

--- client/modules/App/App.js
+++ client/modules/App/App.js
@@ -34,8 +34,8 @@ export class App extends Component {
         {this.state.isMounted && !window.devToolsExtension && process.env.NODE_ENV === 'development' && <DevTools />}
         <div>
           <Helmet
-            title="MERN Starter - Blog App"
-            titleTemplate="%s - Blog App"
+            title="Facekoob"
+            titleTemplate="%s - Facekoob"
             meta={[
               { charset: 'utf-8' },
               {
