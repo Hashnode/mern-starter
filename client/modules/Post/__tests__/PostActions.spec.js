4d496e0844bfcfda9ab2b8f329dde0cb2bf8860d
flystyle <alex.syrotenko.official@gmail.com>
2017-10-17 10:09:23 +0300

    fix tests throgh changes in model

--- client/modules/Post/__tests__/PostActions.spec.js
+++ client/modules/Post/__tests__/PostActions.spec.js
@@ -10,7 +10,9 @@ import {
   addPosts,
 } from '../PostActions';
 
-const post = { name: 'Prashant', title: 'Hello Mern', cuid: 'f34gb2bh24b24b2', content: "All cats meow 'mern!'", slug: 'hello-mern', _id: 1 };
+const post = { name: 'Prashant', title: 'Hello Mern', cuid: 'f34gb2bh24b24b2',
+               content: "All cats meow 'mern!'", slug: 'hello-mern', _id: 1,
+               comments: [{ author: 'Leeroy', text: 'Paladin says "Leeroy Jenkins!"' }] };
 
 test('should return the correct type for addPost', actionTest(
   addPost,
--- server/models/__tests__/post.spec.js
+++ server/models/__tests__/post.spec.js
@@ -6,8 +6,10 @@ import { connectDB, dropDB } from '../../util/test-helpers';
 
 // Initial posts added into test db
 const posts = [
-  new Post({ name: 'Prashant', title: 'Hello Mern', slug: 'hello-mern', cuid: 'f34gb2bh24b24b2', content: "All cats meow 'mern!'" }),
-  new Post({ name: 'Mayank', title: 'Hi Mern', slug: 'hi-mern', cuid: 'f34gb2bh24b24b3', content: "All dogs bark 'mern!'" }),
+  new Post({ name: 'Prashant', title: 'Hello Mern', slug: 'hello-mern', cuid: 'f34gb2bh24b24b2',
+             content: "All cats meow 'mern!'", comments: [] }),
+  new Post({ name: 'Mayank', title: 'Hi Mern', slug: 'hi-mern', cuid: 'f34gb2bh24b24b3',
+             content: "All dogs bark 'merns!'", comments: [{ author: "Yasha", text: "Cool Stuff here!"}] }),
 ];
 
 test.beforeEach('connect and add two post entries', t => {
@@ -36,7 +38,8 @@ test.serial('Should correctly give number of Posts', async t => {
 test.serial('Should send correct data when queried against a cuid', async t => {
   t.plan(2);
 
-  const post = new Post({ name: 'Foo', title: 'bar', slug: 'bar', cuid: 'f34gb2bh24b24b2', content: 'Hello Mern says Foo' });
+  const post = new Post({ name: 'Foo', title: 'bar', slug: 'bar', cuid: 'f34gb2bh24b24b2',
+                          content: 'Hello Mern says Foo', comments: [] });
   post.save();
 
   const res = await request(app)
@@ -64,7 +67,8 @@ test.serial('Should correctly add a post', async t => {
 test.serial('Should correctly delete a post', async t => {
   t.plan(2);
 
-  const post = new Post({ name: 'Foo', title: 'bar', slug: 'bar', cuid: 'f34gb2bh24b24b2', content: 'Hello Mern says Foo' });
+  const post = new Post({ name: 'Foo', title: 'bar', slug: 'bar', cuid: 'f34gb2bh24b24b2',
+                          content: 'Hello Mern says Foo', comments: [] });
   post.save();
 
   const res = await request(app)
@@ -76,4 +80,3 @@ test.serial('Should correctly delete a post', async t => {
   const queriedPost = await Post.findOne({ cuid: post.cuid }).exec();
   t.is(queriedPost, null);
 });
-
