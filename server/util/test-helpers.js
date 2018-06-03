59e3e94d17b45af50619548e28c3dc82964e1a64
Matt Lubner <matt@mattlubner.com>
2018-06-03 14:27:05 -0700

    Use async ava hooks so mockgoose behaves itself

--- server/util/test-helpers.js
+++ server/util/test-helpers.js
@@ -3,17 +3,13 @@ import { Mockgoose } from 'mockgoose';
 
 const mockgoose = new Mockgoose(mongoose);
 
-export function connectDB(t, done) {
-  mockgoose.prepareStorage().then(() => {
-    mongoose.connect('mongodb://localhost:27017/mern-test', err => {
-      if (err) t.fail('Unable to connect to test database');
-      done();
-    });
-  });
+export async function connectDB() {
+  await mockgoose.prepareStorage();
+  await mongoose.connect('mongodb://localhost:27017/mern-test')
+    .catch(() => 'Unable to connect to test database');
 }
 
-export function dropDB(t) {
-  mockgoose.helper.reset(err => {
-    if (err) t.fail('Unable to reset test database');
-  });
+export async function dropDB() {
+  await mockgoose.helper.reset()
+    .catch(() => 'Unable to reset test database');
 }
