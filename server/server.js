ef2080a1b0161fd281f336be4697233ecb525b69
Thorsten van Eyk <thorsten.van-eyk@stud.uni-due.de>
2018-10-03 22:57:52 +1300

    heroku preparation

--- server/server.js
+++ server/server.js
@@ -60,14 +60,16 @@ mongoose.Promise = global.Promise;
 
 // MongoDB Connection
 if (process.env.NODE_ENV !== 'test') {
+  const db = mongoose.connection;
+  db.on('error', console.error);
+  db.once('open', () => {
+    dummyData(); // feed some dummy data in DB.
+  });
   mongoose.connect(serverConfig.mongoURL, (error) => {
     if (error) {
       console.error('Please make sure Mongodb is installed and running!'); // eslint-disable-line no-console
       throw error;
     }
-
-    // feed some dummy data in DB.
-    dummyData();
   });
 }
 
