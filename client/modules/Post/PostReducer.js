25a0e65741b07819b454e7f5760a3ca22e71300f
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2019-01-13 19:28:11 +0200

    Add CREATE_COMMENT case to PostReducer.js file. Turn off no-return-reassign and  no-param-reassign

--- client/modules/Post/PostReducer.js
+++ client/modules/Post/PostReducer.js
@@ -1,4 +1,4 @@
-import { ADD_POST, ADD_POSTS, DELETE_POST } from './PostActions';
+import { ADD_POST, ADD_POSTS, DELETE_POST, CREATE_COMMENT } from './PostActions';
 
 // Initial State
 const initialState = { data: [] };
@@ -7,17 +7,26 @@ const PostReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_POST :
       return {
+        ...state,
         data: [action.post, ...state.data],
       };
 
     case ADD_POSTS :
       return {
+        ...state,
         data: action.posts,
       };
 
     case DELETE_POST :
       return {
+        ...state,
         data: state.data.filter(post => post.cuid !== action.cuid),
+      };
+
+    case CREATE_COMMENT :
+      return {
+        ...state,
+        data: state.data.map(item => item._id === action.post._id ? item = action.post : item),
       };
 
     default:
