33c329fed4789ced9fbe5806eb8b271ca6891419
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2019-01-13 20:18:33 +0200

    In the PostList component add to PostListItem component onCreateComment, onEditComment, onDeleteComment, showAddComment, showEditComment properties

--- client/modules/Post/components/PostList.js
+++ client/modules/Post/components/PostList.js
@@ -5,30 +5,51 @@ import PropTypes from 'prop-types';
 import PostListItem from './PostListItem/PostListItem';
 
 function PostList(props) {
+  const {
+    posts,
+    handleDeletePost,
+    handleCreateComment,
+    handleDeleteComment,
+    handleEditComment,
+    showAddComment,
+    showEditComment,
+  } = props;
   return (
     <div className="listView">
-      {
-        props.posts.map(post => (
-          <PostListItem
-            post={post}
-            key={post.cuid}
-            onDelete={() => props.handleDeletePost(post.cuid)}
-          />
-        ))
-      }
+      {posts.map((post) => (
+        <PostListItem
+          post={post}
+          comments={post.comments}
+          key={post.cuid}
+          onDelete={() => handleDeletePost(post.cuid)}
+          onCreateComment={handleCreateComment}
+          onEditComment={handleEditComment}
+          onDeleteComment={handleDeleteComment}
+          showAddComment={showAddComment}
+          showEditComment={showEditComment}
+        />
+      ))}
     </div>
   );
 }
 
 PostList.propTypes = {
-  posts: PropTypes.arrayOf(PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    content: PropTypes.string.isRequired,
-    slug: PropTypes.string.isRequired,
-    cuid: PropTypes.string.isRequired,
-  })).isRequired,
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      content: PropTypes.string.isRequired,
+      slug: PropTypes.string.isRequired,
+      cuid: PropTypes.string.isRequired,
+    })
+  ).isRequired,
   handleDeletePost: PropTypes.func.isRequired,
+  handleCreateComment: PropTypes.func.isRequired,
+  handleEditComment: PropTypes.func.isRequired,
+  handleDeleteComment: PropTypes.func.isRequired,
+  showAddComment: PropTypes.bool.isRequired,
+  showEditComment: PropTypes.bool.isRequired,
 };
 
 export default PostList;
+
