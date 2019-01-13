78398b79509be5d3c322bf1c6256b268d78384a9
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2019-01-13 19:11:20 +0200

    Add to project CommentListItem component

---
+++ client/modules/Post/components/CommentListItem/CommentListItem.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { FormattedMessage } from 'react-intl';
+import { Link } from 'react-router';
+
+// Import Style
+import styles from './CommentListItem.css';
+
+function CommentListItem(props) {
+  const { comment : {name, text}, postID, onDeleteComment, onEditComment, showAddComment, showEditComment } = props;
+  return (
+    <React.Fragment>
+      <div className={styles['single-comment']}>
+        <p className={styles['author-name']}>
+          <FormattedMessage id="by" /> {name}
+        </p>
+        <p className={styles['comment-desc']}>{text}</p>
+        <p className={styles['post-action']}>
+          <a href="#" onClick={onDeleteComment}>
+            <FormattedMessage id="deleteComment" />
+          </a>
+        </p>
+        <p className={styles['post-action']}>
+          <Link to={`/posts/${postID}`} onClick={onEditComment}>
+            <FormattedMessage id={`${showAddComment || showEditComment ? "hideCommentForm":"editComment"}`} />
+          </Link>
+        </p>
+      </div>
+      {/* <hr className={styles.divider} /> */}
+    </React.Fragment>
+  );
+}
+
+CommentListItem.propTypes = {
+  comment: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    text: PropTypes.string.isRequired,
+  }).isRequired,
+  onDeleteComment: PropTypes.func.isRequired
+};
+
+export default CommentListItem;
