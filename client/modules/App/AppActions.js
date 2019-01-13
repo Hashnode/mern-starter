ff7c6e3f7bd3aeb3322751ddddb58c1d751eb342
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2019-01-13 20:44:54 +0200

    Add to AppActions.js file toggleAddComment and toggleEditComment functions

--- client/modules/App/AppActions.js
+++ client/modules/App/AppActions.js
@@ -1,9 +1,21 @@
 // Export Constants
 export const TOGGLE_ADD_POST = 'TOGGLE_ADD_POST';
+export const TOGGLE_ADD_COMMENT = 'TOGGLE_ADD_COMMENT';
+export const TOGGLE_EDIT_COMMENT = 'TOGGLE_EDIT_COMMENT';
 
 // Export Actions
 export function toggleAddPost() {
   return {
     type: TOGGLE_ADD_POST,
+  };
+}
+export function toggleAddComment() {
+  return {
+    type: TOGGLE_ADD_COMMENT,
+  };
+}
+export function toggleEditComment() {
+  return {
+    type: TOGGLE_EDIT_COMMENT,
   };
 }
