c3a343e640d2ca82f8f67d51eaa3832a7bb80aed
Konstantin Dergachev <dergachevkonstantin@gmail.com>
2019-01-13 20:48:05 +0200

    Add to AppReducer.js file TOGGLE_ADD_COMMENT and TOGGLE_EDIT_COMMENT cases. Also Add getShowaddComment and getShowEditComment functions

--- client/modules/App/AppReducer.js
+++ client/modules/App/AppReducer.js
@@ -1,9 +1,11 @@
 // Import Actions
-import { TOGGLE_ADD_POST } from './AppActions';
+import { TOGGLE_ADD_POST, TOGGLE_ADD_COMMENT, TOGGLE_EDIT_COMMENT } from './AppActions';
 
 // Initial State
 const initialState = {
   showAddPost: false,
+  showAddComment: false,
+  showEditComment: false,
 };
 
 const AppReducer = (state = initialState, action) => {
@@ -11,6 +13,20 @@ const AppReducer = (state = initialState, action) => {
     case TOGGLE_ADD_POST:
       return {
         showAddPost: !state.showAddPost,
+      };
+    case TOGGLE_ADD_COMMENT:
+      return {
+        ...state,
+        showAddPost: false,
+        showAddComment: !state.showAddComment,
+        showEditComment: false,
+      };
+    case TOGGLE_EDIT_COMMENT:
+      return {
+        ...state,
+        showAddPost: false,
+        showAddComment: false,
+        showEditComment: !state.showEditComment,
       };
 
     default:
@@ -22,6 +38,10 @@ const AppReducer = (state = initialState, action) => {
 
 // Get showAddPost
 export const getShowAddPost = state => state.app.showAddPost;
+// Get showAddComment
+export const getShowAddComment = state => state.app.showAddComment;
+// Get showEditComment
+export const getShowEditComment = state => state.app.showEditComment;
 
 // Export Reducer
 export default AppReducer;
