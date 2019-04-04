0812e1bf02330995f1af013cbc0b0e23cc93fc61
Valerii Shelihan <valerasheligan@gmail.com>
2019-04-05 01:15:47 +0300

    add CommentList component. add CommentReducer to combineRecusers func

--- client/reducers.js
+++ client/reducers.js
@@ -7,10 +7,12 @@ import { combineReducers } from 'redux';
 import app from './modules/App/AppReducer';
 import posts from './modules/Post/PostReducer';
 import intl from './modules/Intl/IntlReducer';
+import comments from './modules/Comment/CommentReducer';
 
 // Combine all reducers into one root reducer
 export default combineReducers({
   app,
   posts,
   intl,
+  comments,
 });
