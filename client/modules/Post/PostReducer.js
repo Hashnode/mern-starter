8925dae1033cf370868715e4ddc3527f951722c1
Bohdan Kucheriavyi <kucheriavyi.w32@gmail.com>
2019-08-04 19:37:33 +0300

    New reduce (EDIT_POST) for PostReducer.js

--- client/modules/Post/PostReducer.js
+++ client/modules/Post/PostReducer.js
@@ -1,23 +1,28 @@
-import { ADD_POST, ADD_POSTS, DELETE_POST } from './PostActions';
+import { ADD_POST, ADD_POSTS, DELETE_POST, EDIT_POST } from './PostActions';
 
 // Initial State
 const initialState = { data: [] };
 
 const PostReducer = (state = initialState, action) => {
   switch (action.type) {
-    case ADD_POST :
+    case ADD_POST:
       return {
         data: [action.post, ...state.data],
       };
 
-    case ADD_POSTS :
+    case ADD_POSTS:
       return {
         data: action.posts,
       };
 
-    case DELETE_POST :
+    case DELETE_POST:
       return {
         data: state.data.filter(post => post.cuid !== action.cuid),
+      };
+
+    case EDIT_POST:
+      return {
+        data: [...state.data],
       };
 
     default:
