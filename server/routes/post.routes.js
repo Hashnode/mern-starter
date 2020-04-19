e1dda5a4518f376c55469741bcbfe7a7854b656d
avadadevelopers <dev@avada-media.com>
2020-04-19 21:26:01 +0300

    added comment routes to post routes

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import * as PostController from '../controllers/post.controller';
+import postCommentRouter from './postComment.routes';
+
 const router = new Router();
 
 // Get all Posts
@@ -13,5 +15,8 @@ router.route('/posts').post(PostController.addPost);
 
 // Delete a post by cuid
 router.route('/posts/:cuid').delete(PostController.deletePost);
+
+// Routes for comments
+router.use('/posts/:cuid/comment', postCommentRouter);
 
 export default router;
