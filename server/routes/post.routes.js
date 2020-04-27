4024a1c75de13c5d8ed21cea4fa48def4db0dd7e
LevonHaroutyunyan <levon.haroutyunyan@gmail.com>
2020-04-27 19:54:42 +0400

    .

--- server/routes/post.routes.js
+++ server/routes/post.routes.js
@@ -14,4 +14,13 @@ router.route('/posts').post(PostController.addPost);
 // Delete a post by cuid
 router.route('/posts/:cuid').delete(PostController.deletePost);
 
+//add Comment a post by cuid
+router.route('/posts/:cuid').put(PostController.addComment);
+
+//edit Comment
+router.route('/posts/edit/:cuid').put(PostController.editComment);
+
+//delete Comment
+router.route('/posts/delete/:cuid').delete(PostController.editComment);
+
 export default router;
